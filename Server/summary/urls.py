67e7c4fc3e3d535d2c0361733da3c9a57aae371e
Zaki Shaikh <zaki.shaikh.o2o@gmail.com>
2019-08-10 15:49:21 +0530

    Final

---
+++ Server/summary/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import views as summary_views
+from django.conf.urls import url
+
+urlpatterns = [
+	url(r'^$', summary_views.index, name = 'index'),
+	url(r'^homepage/$',summary_views.homepage),
+	
+]
